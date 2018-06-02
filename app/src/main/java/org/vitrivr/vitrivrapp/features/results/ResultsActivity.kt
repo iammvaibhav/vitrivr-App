2699e44a74884c45d4c9cf598b30a8d3b6880925
Vaibhav Maheshwari <iammvaibhav@gmail.com>
2018-06-02 14:54:58 +0530

    Setup basic ResultActivity layout

---
+++ app/src/main/java/org/vitrivr/vitrivrapp/features/results/ResultsActivity.kt
@@ -0,0 +1,12 @@
+package org.vitrivr.vitrivrapp.features.results
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ResultsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
